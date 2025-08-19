baeb64837f603ba6fb355ac76647feb901e11d0c
msong254 <msong254@uwo.ca>
2025-08-19 15:06:58 -0600

    fix: force PostgreSQL connection for production

--- database/db.py
+++ database/db.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:////app/server/db/app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://microsmaximizing_db_user:3HcvTPa8IBsEyBJVzycJ3O7m78PsBWJ4@dpg-d2ie60emcj7s738df770-a.oregon-postgres.render.com/microsmaximizing_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
